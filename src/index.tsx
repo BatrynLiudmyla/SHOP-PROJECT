339d7f6e8e4d2992052634ad1e77c6c1983448b0
Liudmyla <diklzbatrin@gmail.com>
2023-02-12 17:37:09 +0200

    remove files and add absolute importвидалити файли та додати абсолютний імпорт

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,7 @@
-import { type } from 'os'
-import { title } from 'process'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Content} from './Content'
-import Title from './Title'
-
-
-function App() {
-    return (
-        <>
-            <Title text="React" />
-            <Title text="TS" />
-            <Content text1="Hello world 1" text2="Hello world 2" year={2023} />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <React.StrictMode> </React.StrictMode>
 )
