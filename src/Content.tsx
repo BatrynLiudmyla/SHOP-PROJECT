df92ec6832da66be35211b7188a26f3de0ee81d7
Liudmyla <diklzbatrin@gmail.com>
2023-02-12 17:23:52 +0200

    Content scss

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,14 +1,16 @@
-import classes from "./Content.module.css"
+import "./Content.scss"
 
 type Props = {
   text1: string
   text2: string
   year: number
 }
- export const Content = (props: Props) => {
+
+
+export const Content = (props: Props) => {
            return (
           <>
-              <p className={`${classes.text}`}>{props.text1}</p>
+              <p className="text">{props.text1}</p>
               <p>{props.text2}</p>
               <div>Year: {props.year}</div>
           </>
