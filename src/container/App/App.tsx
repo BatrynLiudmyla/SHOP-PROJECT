04f83d88374044a17a60151b82fbdd9ebbaf2d46
Liudmyla <diklzbatrin@gmail.com>
2023-02-15 09:39:40 +0200

    Пфдключення Css до App

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -2,6 +2,7 @@ import Footer from "container/Footer/Footer"
 import Header from "container/Header/Header"
 import Main from "container/Main/Main"
 import CssBaseline from '@mui/material/CssBaseline';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 
 
@@ -9,13 +10,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 type Props = {}
 const App = (props: Props) => {
   return ( 
-  <>
+    <StyledEngineProvider injectFirst>
   <CssBaseline/>
   <Header/>
   <Main/>
   <Footer/>
 
-  </>
+  </StyledEngineProvider>
   )
 }
 export default App
