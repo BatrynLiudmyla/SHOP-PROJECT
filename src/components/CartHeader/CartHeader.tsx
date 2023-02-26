6de7a5168ec1321731c86d6df6bdef513f5d7d05
Liudmyla <diklzbatrin@gmail.com>
2023-02-26 20:45:52 +0200

    add React Router and connect to app

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,20 +1,28 @@
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
+
 type Props = {
-  productsInCart: {
-    [id: number]: number
-  }
+    productsInCart: {
+        [id: number]: number
+    }
+
+    productsObject?: {
+        [id: number]: Product
+    }
 }
 
-const CartHeader = ({ productsInCart }: Props) => {
-  console.log(Object.keys(productsInCart))
-  return (
-    <div>
-      {Object.keys(productsInCart).map((productId) => (
-        <div key = {productId}>
-          {productId} : {productsInCart[parseInt(productId)]}
-          </div>
-      ))}
-        
-    </div>
-  )
+const CartHeader = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
+    return (
+        <div>
+            {Object.keys(productsInCart).map((productId) => (
+                <div key={productId}>
+                    {productsObject[parseInt(productId)].title} :{' '}
+                    {productsInCart[parseInt(productId)]}
+                </div>
+            ))}
+        </div>
+    )
 }
 export default CartHeader
